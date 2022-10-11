a2fccc721fc8180dc9577ca182141b2a9fab6cf0
AkifHaciyev <akif6563@mail.ru>
2022-10-12 02:27:04 +0400

    add login & validation

--- src/layouts/login.jsx
+++ src/layouts/login.jsx
@@ -1,7 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import TextField from "../components/textField";
+import { validator } from "../utils/validator";
 
 const Login = () => {
-    return <div>Login</div>;
+    const [data, setData] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({});
+
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+
+    const validatorConfig = {
+        email: {
+            isRequired: {
+                message: "Электронная почта обязательна для заполнения"
+            },
+            isEmail: {
+                message: "Email веден некорректно"
+            }
+        },
+        password: {
+            isRequired: {
+                message: "Пароль обязателен для заполнения"
+            },
+            isCapitalSymbol: {
+                message: "Пароль должен содержать заглавную букву"
+            },
+            isContainDigit: {
+                message: "Пароль должен содержать одно число"
+            },
+            min: {
+                message: "Пароль должен содержать 8 символов",
+                value: 8
+            }
+        }
+    };
+
+    const validate = () => {
+        const errors = validator(data, validatorConfig);
+        setErrors(errors);
+        return Object.keys(errors).length === 0;
+    };
+
+    useEffect(() => {
+        validate();
+    }, [data]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        validate();
+        const isValid = validate();
+        if (!isValid) return;
+        console.log(data);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <TextField
+                lable="почта"
+                name="email"
+                value={data.email}
+                onChange={handleChange}
+                error={errors.email}
+            />
+            <TextField
+                lable="Пароль"
+                type="password"
+                name="password"
+                value={data.password}
+                onChange={handleChange}
+                error={errors.password}
+            />
+            <button type="submit">Submit</button>
+        </form>
+    );
 };
 
 export default Login;
